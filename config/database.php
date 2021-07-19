106d9d8cf523fa9763ea55c22aa1969478ad32ee
kevinron0148 <kevinron01@hotmail.com>
2021-07-19 16:05:55 -0500

    Update database.php

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+    class Database{
+
+        private static $DBConnection;
+  
+        public static function connectDB(){
+            if(self::$DBConnection === null){ // Database::DBConnection
+                self::$DBConnection = new PDO('mysql:host=127.0.0.1;dbname=api;', 'root', 'Syst3m4$');
+                self::$DBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$DBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            }
+
+            return self::$DBConnection;
+        }
+    }
