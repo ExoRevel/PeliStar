c288772f84f0df7bd36a040ed4c9f06c2527f3e4
kevinron0148 <kevinron01@hotmail.com>
2021-07-19 16:33:53 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -6,8 +6,8 @@
   
         public static function connectDB(){
             if(self::$DBConnection === null){ // Database::DBConnection
-                self::$DBConnection = new PDO('mysql:host=127.0.0.1;dbname=api;', 'root', 'Syst3m4$');
-                self::$DBConnection = new PDO('mysql:host=127.0.0.1;dbname=api_rest;', 'root', '123456');
+                self::$DBConnection = new PDO('mysql:host=127.0.0.1;dbname=pelistar;', 'root', 'Syst3m4$');
+                self::$DBConnection = new PDO('mysql:host=127.0.0.1;dbname=pelistar;', 'root', '123456');
                 self::$DBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$DBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             }
