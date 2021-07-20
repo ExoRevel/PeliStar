f6c60623476e09504c344a0aec2e417c6307cc7d
ExoRevel <61420550+ExoRevel@users.noreply.github.com>
2021-07-20 17:06:50 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@
   
         public static function connectDB(){
             if(self::$DBConnection === null){ // Database::DBConnection
-                self::$DBConnection = new PDO('mysql:host=127.0.0.1;dbname=pelistar;', 'root', 'Syst3m4$');
+                //self::$DBConnection = new PDO('mysql:host=127.0.0.1;dbname=pelistar;', 'root', 'Syst3m4$');
                 self::$DBConnection = new PDO('mysql:host=127.0.0.1;dbname=pelistar;', 'root', '123456');
                 self::$DBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 self::$DBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
