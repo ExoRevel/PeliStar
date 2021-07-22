6922f2c558dc5282604b5d3e9afeef6f763abd16
ExoRevel <61420550+ExoRevel@users.noreply.github.com>
2021-07-22 15:27:25 -0500

    POST Movie_directors

--- controller/movie_directors.php
+++ controller/movie_directors.php
@@ -1,7 +1,7 @@
 <?php
 
     require_once('../config/database.php');   
-    require_once('../data/Fav_moviesDB.php');
+    require_once('../data/Movies_Directors.php');
     require_once('../util/response.php');
 
     //Respuesta que se enviará al cliente    
@@ -24,6 +24,7 @@
         $response->sendParams(true, 200);
     }
 
+
     switch($_SERVER['REQUEST_METHOD']){
 
         case 'POST':
@@ -37,44 +38,43 @@
                 $response->sendParams(false, 400, 'Body no es Válido (JSON)');
             }
 
-            if( !isset($jsonData->MOVIE_ID) || !isset($jsonData->ACTOR_ID)){
+            if( !isset($jsonData->MOVIEDIRECTOR_ID) || !isset($jsonData->MOVIE_ID)){
                 $messages = array();
 
-                (!isset($jsonData->MOVIE_ID) ? $messages[] = 'MOVIE_ID no ingresado': false);
-                (!isset($jsonData->ACTOR_ID) ? $messages[] = 'Campo ACTOR_ID no ingresado': false);
+                (!isset($jsonData->MOVIEDIRECTOR_ID) ? $messages[] = 'MOVIEDIRECTOR_ID no ingresado': false);
+                (!isset($jsonData->MOVIE_ID) ? $messages[] = 'Campo MOVIE_ID no ingresado': false);
 
                 $response->sendParams(false,400, $messages);
 
             }
 
+            $MOVIEDIRECTOR_ID = trim($jsonData->MOVIEDIRECTOR_ID);
             $MOVIE_ID = trim($jsonData->MOVIE_ID);
-            $ACTOR_ID = trim($jsonData->ACTOR_ID);
         
             try{
                 
-                $movie_actor = new Movie_actorsDB($MOVIE_ID, $ACTOR_ID);
-                $rowCount = $movie_actorDB->insertar($movie_actorDB);
+                $movie_director = new Movie_directorDB($MOVIEDIRECTOR_ID, $MOVIE_ID);
+                $rowCount = $movie_directorDB->insertar($movie_directorDB);
 
                 if($rowCount === 0){
-                    $response->sendParams(false, 404, 'Hubo un error al recuperar el MOVIE_ACTOR creado');
+                    $response->sendParams(false, 404, 'Hubo un error al recuperar el FAV_MOVIE creado');
                 }
 
                 $returnData = array();
                 $returnData['rows_returned'] = $rowCount;
-                $returnData['movie_actors'] = $lastmovie_actors;
+                $returnData['movie_director'] = $lastmovie_directors;
 
-                $response->sendParams(true, 201, 'MOVIE_ACTOR insertado correctamente', $returnData);
+                $response->sendParams(true, 201, 'movie_director insertado correctamente', $returnData);
 
             }
 
-            catch(Movies_ActorsException $ex){
+            catch(Movies_DirectorsException $ex){
                 $response->sendParams(false, 400, $ex->getMessage());
             }
             catch(PDOException $ex){
                 error_log("Database query error - {$ex}", 0);
-                $response->sendParams(false, 500, 'Error al insertar MOVIE_ACTOR');
+                $response->sendParams(false, 500, 'Error al insertar MOVIE_DIrectors');
             }
             break;
 
     }
-
