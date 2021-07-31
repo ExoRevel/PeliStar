8fd7f21b6f29fdcab3f695b91091196a4ae732f7
kevinron0148 <kevinron01@hotmail.com>
2021-07-30 21:02:08 -0500

    PATCH ACTORS

--- controller/actors.php
+++ controller/actors.php
@@ -18,7 +18,7 @@
     header('Access-Control-Allow-Origin: *');
     //Opciones de preflight (CORS)
     if($_SERVER['REQUEST_METHOD'] === 'OPTIONS'){
-        header('Access-Control-Allow-Methods: POST, OPTIONS, GET');
+        header('Access-Control-Allow-Methods: POST, OPTIONS, GET, PATCH');
         header('Access-Control-Allow-Headers: Content-Type, Authorization');
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Max-Age: 86400');
@@ -37,11 +37,11 @@
                 $response->sendParams(false, 400, 'Body no es Válido (JSON)');
             }
 
-            if( !isset($jsonData->ACTOR_FULLNAME) || !isset($jsonData->ACTOR_BIRTHDAY)){
+            if( !($jsonData->ACTOR_FULLNAME) || !($jsonData->ACTOR_BIRTHDAY)){
                 $messages = array();
 
-                (!isset($jsonData->ACTOR_FULLNAME) ? $messages[] = 'EL NOMBRE DEL ACTOR NO FUE INGRESADO': false);
-                (!isset($jsonData->ACTOR_BIRTHDAY) ? $messages[] = 'LA FECHA DE NACIMIENTO DEL ACTOR NO FUE INGRESADA': false);
+                (!($jsonData->ACTOR_FULLNAME) ? $messages[] = 'EL NOMBRE DEL ACTOR NO FUE INGRESADO': false);
+                (!($jsonData->ACTOR_BIRTHDAY) ? $messages[] = 'LA FECHA DE NACIMIENTO DEL ACTOR NO FUE INGRESADA': false);
 
                 $response->sendParams(false,400, $messages);
 
@@ -56,7 +56,7 @@
                 $rowCount = count($existingactor);
 
                 if($rowCount !==0){
-                    $response->sendParams(false, 409, 'ACTOR ya existe en la base de data');
+                    $response->sendParams(false, 409, 'Este actor ya se encuentra registrado');
                 }
                 
                 $actor = new Actors(null, $ACTOR_FULLNAME, $ACTOR_BIRTHDAY);
@@ -101,7 +101,7 @@
                     }
                     catch(PDOException $ex){
                         error_log("Database query error - {$ex}", 0);
-                        $response->sendParams(false, 500);
+                        $response->sendParams(false, 500,"ERROR DE BASE DE DATOS");
                     }
                 }
 
@@ -123,8 +123,65 @@
                     }
                     catch(PDOException $ex){
                         error_log("Database query error - {$ex}", 0);
-                        $response->sendParams(false, 500);
+                        $response->sendParams(false, 500,"ERROR DE BASE DE DATOS");
                     }
+                }
+                break;
+            case 'PATCH':
+                if($_SERVER['CONTENT_TYPE']!== 'application/json'){
+                    $response->sendParams(false, 400, 'Content Type header no Válido');
+                }
+    
+                $rawPostData = file_get_contents('php://input');
+    
+                if(!$jsonData = json_decode($rawPostData)){
+                    $response->sendParams(false, 400, 'Body no es Válido (JSON)');
+                }
+    
+                $ACTOR_FULLNAME = trim($jsonData->ACTOR_FULLNAME);
+                $ACTOR_BIRTHDAY = trim($jsonData->ACTOR_BIRTHDAY);
+                $ACTOR_ID = $jsonData->ACTOR_ID;
+                try{
+                    $actorDB = new ActorDB($database);
+                    $existingactor = $actorDB ->obtenerPorId($ACTOR_ID);
+                    $rowCount = count($existingactor);
+                    if($rowCount === 0){
+                        $response->sendParams(false, 409, 'Este ACTOR no existe en la base de data');
+                    }
+                    $actor = new Actors($ACTOR_ID, $ACTOR_FULLNAME, $ACTOR_BIRTHDAY);
+                    if($ACTOR_FULLNAME!=null)
+                    {
+                        $rowCount = $actorDB->actualizarFullnamePorId($ACTOR_FULLNAME, $ACTOR_ID);
+                    }
+
+                    if($rowCount === 0){
+                        $response->sendParams(false, 404, 'ERROR! Ingrese un nombre diferente al registrado, o deje la casilla en blanco');
+                    }
+
+                    if($ACTOR_BIRTHDAY!=null)
+                    {
+                        $rowCount = $actorDB->actualizarBirthdayPorId($actor);
+                    }
+                    
+                    if($rowCount === 0){
+                        $response->sendParams(false, 404, 'ERROR! Ingrese un fecha diferente a la registrada, o deje la casilla en blanco');
+                    }
+                    if( !($jsonData->ACTOR_FULLNAME) && !($jsonData->ACTOR_BIRTHDAY)){
+                        $messages = array();
+                        (!($jsonData->ACTOR_FULLNAME) ? $messages[] = 'EL NOMBRE DEL ACTOR NO FUE INGRESADO': false);
+                        (!($jsonData->ACTOR_BIRTHDAY) ? $messages[] = 'LA FECHA DE NACIMIENTO DEL ACTOR NO FUE INGRESADA': false);
+                        $response->sendParams(false,400, $messages);
+                    }
+
+                    $returnData = array();
+                    $response->sendParams(true, 201, 'Los datos fueron modificados correctamente', $returnData);
+                }
+                catch(ActorsException $ex){
+                    $response->sendParams(false, 400, $ex->getMessage());
+                }
+                catch(PDOException $ex){
+                    error_log("Database query error - {$ex}", 0);
+                    $response->sendParams(false, 500, "ERROR DE BASE DE DATOS");
                 }
                 break;
             default: 
--- data/ActorDB.php
+++ data/ActorDB.php
@@ -69,5 +69,56 @@
             return $rowCount;
         }
 
+        public function actualizarDatos($actor)
+        {
+            $paramNumber = 0;
+            $queryFields = '';
+            $FULLNAME = $actor->getFullname();
+            $BIRTHDAY = $actor->getBirthday();
+            $ACTOR_ID = $actor->getId();
+            if($FULLNAME!=null){ //Se verifica que campo fue ingresado
+                $queryFields .= 'ACTOR_FULLNAME = ?, '; 
+            }
+            if($BIRTHDAY!=null){ //Se verifica que campo fue ingresado
+                $queryFields .= 'ACTOR_BIRTHDAY = ?, ';   
+            }      
+            $query = $this->database->prepare('UPDATE Actors SET {$queryFields}  WHERE ACTOR_ID = ?');
+            if($FULLNAME!==null){ 
+                $query->bindParam(3, $FULLNAME, PDO::PARAM_STR);
+            }
+            if($BIRTHDAY!==null){ 
+                $query->bindParam(2, $BIRTHDAY, PDO::PARAM_STR);
+            }
+            $query->bindParam(3, $FULLNAME, PDO::PARAM_STR);
+            $query->bindParam(2, $BIRTHDAY, PDO::PARAM_STR);
+            $query->bindParam(1, $ACTOR_ID, PDO::PARAM_STR);
+            //$query->bindParam($paramNumber++, $BIRTHDAY, PDO::PARAM_STR);          
+            $query->execute();
+            $rowCount = $query->rowCount();
+
+            return $rowCount;
+        }
+
+        public function actualizarFullnamePorId($FULLNAME,$ACTOR_ID)
+        {
+            $query = $this->database->prepare('UPDATE Actors SET  ACTOR_FULLNAME = ? WHERE ACTOR_ID = ?');
+            $query->bindParam(1, $FULLNAME, PDO::PARAM_STR);
+            $query->bindParam(2, $ACTOR_ID, PDO::PARAM_STR);
+            $query ->execute();
+            $rowCount = $query->rowCount();
+            return $rowCount;
+        }
+
+        public function actualizarBirthdayPorId($actor){
+            $BIRTHDAY = $actor->getBirthday();
+            $ACTOR_ID = $actor->getId();
+            $query = $this->database->prepare('UPDATE Actors SET  ACTOR_BIRTHDAY = ? WHERE ACTOR_ID = ?');
+            $query->bindParam(1, $BIRTHDAY, PDO::PARAM_STR);
+            $query->bindParam(2, $ACTOR_ID, PDO::PARAM_STR);
+            $query ->execute();
+            $rowCount = $query->rowCount();
+            return $rowCount;
+        }
+
 
     }
